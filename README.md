d938149b21e4d7ad18527496fb3c58b63af808e4
Eli Dross <Logandr2@gmail.com>
2024-02-07 20:03:51 -0500

    completley changing to a fantasy football data analysis

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # INST414-ModuleAssignment1
-# Eli Dross
-# This is a readme file
-# 
-# The csv in this repositiory is data on netflix stocks during 2023 and January 2024
-# The python file in this repositiory does a data analysis on the netflix csv data
+### 
+### Eli Dross
+### This is a readme file
+### 
+### The two csv's in this repositiory is data on fantasy football in 2022 
+### 
+### The python file in this repositiory does a data analysis on the two csv 
+### datasets
--- data_analysis.py
+++ data_analysis.py
@@ -1,30 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
-#Loads the dataset
-netflix_data = pd.read_csv('Netflix_data.csv')
 
-#Removes rows with no date
-netflix_data = netflix_data.dropna(subset=['Date'])
+#Loads the datasets
+adp_data = pd.read_csv("adp_data.csv")
+weekly_data = pd.read_csv("adp_data.csv")
 
-#Displays basic statistics of the netflix data
-print(netflix_data.describe())
-plt.figure(figsize=(12, 6))
-plt.plot(netflix_data['Date'], netflix_data['Close'], color='red')
-plt.title('Netflix Stock Price (2023-24)')
-plt.xlabel('Date')
-plt.ylabel('Closing Price (USD)')
-plt.grid(True)
-plt.show()
-
-netflix_high_sorted = netflix_data.sort_values(by='High', ascending=False)
-
-#Displays the top ten high dates in the data set to get a better idea of the 
-#best days for netflix and look for patterns
-print(netflix_high_sorted.head(10))
-
-netflix_low_sorted = netflix_data.sort_values(by='Low', ascending=True)
-
-#Displays the bottom ten dates in the data set to get a better idea of the best 
-#days for netflix and look for patterns
-print(netflix_low_sorted.head(10))
